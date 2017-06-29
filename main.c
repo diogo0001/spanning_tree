5f1f6248c110acfcbdc6e03769d371004b7cb825
diogo0001 <dgtcookmusic@gmail.com>
2017-06-29 17:30:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "In_Out_file.h"
-#include "lista_enc/lista_enc.h"
-#include "lista_enc/no.h"
-#include "fila/fila.h"
-#include "grafo/grafo.h"
-#include "arvore/sub_arvore.h"
+#include "lista_enc.h"
+#include "no.h"
+#include "fila.h"
+#include "grafo.h"
+#include "sub_arvore.h"
 #include <time.h>
 #include <limits.h>
 #include <windows.h>
